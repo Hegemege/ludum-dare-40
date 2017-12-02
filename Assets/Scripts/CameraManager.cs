e0c41bcae21ea494fdd02589bfcb9ae5ce2f20c7
Henri Niva <henri.niva@aalto.fi>
2017-12-02 13:48:48 +0200

    Camera setup and movement

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject MainQuad;
+
+    public GameObject AuxVerticalQuad;
+    public GameObject AuxVerticalCamera;
+
+    public GameObject AuxHorizontalQuad;
+    public GameObject AuxHorizontalCamera;
+
+    public GameObject AuxDiagonalQuad;
+    public GameObject AuxDiagonalCamera;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Move main camera
+
+        var realPosition = MainCamera.transform.localPosition;
+        realPosition.y -= 0.01f;
+
+        var realQuadPosition = realPosition;
+        realQuadPosition.y = -0.001f;
+
+        // Update auxiliary camera positions
+        var verticalSign = realPosition.x > 0f ? -1f : 1f;
+        AuxVerticalCamera.transform.localPosition = realPosition + verticalSign * Vector3.right * 8.999f;
+        AuxVerticalQuad.transform.localPosition = realQuadPosition;
+
+        var horizontalSign = realPosition.z > 0f ? -1f : 1f;
+        AuxHorizontalCamera.transform.localPosition = realPosition + horizontalSign * Vector3.forward * 15.999f;
+        AuxHorizontalQuad.transform.localPosition = realQuadPosition;
+
+        var diagonalXSign = realPosition.x > 0f ? -1f : 1f;
+        var diagonalZSign = realPosition.z > 0f ? -1f : 1f;
+        AuxDiagonalCamera.transform.localPosition = realPosition + diagonalXSign * Vector3.right * 8.999f + diagonalZSign * Vector3.forward * 15.999f;
+        AuxDiagonalQuad.transform.localPosition = realQuadPosition;
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraMovement : MonoBehaviour 
+{
+    void FixedUpdate()
+    {
+        var dt = Time.fixedDeltaTime;
+
+        dt *= 5f;
+
+        if (Input.GetKey("a"))
+        {
+            transform.localPosition += new Vector3(0f, 0f, -1f) * dt;
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.localPosition += new Vector3(-1f, 0f, 0f) * dt;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.localPosition += new Vector3(1f, 0f, 0f) * dt;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.localPosition += new Vector3(0f, 0f, 1f) * dt;
+        }
+
+        if (transform.localPosition.x > 4.5f)
+        {
+            transform.localPosition -= 9f * Vector3.right;
+        }
+
+        if (transform.localPosition.x < -4.5f)
+        {
+            transform.localPosition += 9f * Vector3.right;
+        }
+
+        if (transform.localPosition.z > 8f)
+        {
+            transform.localPosition -= 16f * Vector3.forward;
+        }
+
+        if (transform.localPosition.z < -8f)
+        {
+            transform.localPosition += 16f * Vector3.forward;
+        }
+    }
+}
