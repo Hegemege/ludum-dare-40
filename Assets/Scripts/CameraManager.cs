77bee4c6c178720ea9879687a444fd8251363f68
Henri Niva <henri.niva@aalto.fi>
2017-12-02 15:27:31 +0200

    Tweaking materials and stuff

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -16,9 +16,15 @@ public class CameraManager : MonoBehaviour
     public GameObject AuxDiagonalQuad;
     public GameObject AuxDiagonalCamera;
 
+    private Camera _verticalCamera;
+    private Camera _horizontalCamera;
+    private Camera _diagonalCamera;
+
     void Awake()
     {
-        
+        _verticalCamera = AuxVerticalCamera.GetComponent<Camera>();
+        _horizontalCamera = AuxHorizontalCamera.GetComponent<Camera>();
+        _diagonalCamera = AuxDiagonalCamera.GetComponent<Camera>();
     }
 
     void Update()
@@ -49,5 +55,55 @@ public class CameraManager : MonoBehaviour
         var diagonalZSign = realPosition.z > 0f ? -1f : 1f;
         AuxDiagonalCamera.transform.localPosition = realPosition + diagonalXSign * Vector3.right * 8.999f + diagonalZSign * Vector3.forward * 15.999f;
         AuxDiagonalQuad.transform.localPosition = realQuadPosition;
+
+        // Update auxiliary camera rendering order
+        // The quad closest to the main camera gets highest priority, -2, then -3 and -4
+        // There are only 3 quads so lets just hardcode them
+        var verticalDistance = Vector3.Distance(MainCamera.transform.localPosition, AuxVerticalQuad.transform.localPosition);
+        var horizontalDistance = Vector3.Distance(MainCamera.transform.localPosition, AuxHorizontalQuad.transform.localPosition);
+        var diagonalDistance = Vector3.Distance(MainCamera.transform.localPosition, AuxDiagonalQuad.transform.localPosition);
+
+        if (verticalDistance < horizontalDistance && horizontalDistance < diagonalDistance)
+        {
+            _verticalCamera.depth = -2;
+            _horizontalCamera.depth = -3;
+            _diagonalCamera.depth = -4;
+        }
+
+        if (verticalDistance < diagonalDistance && diagonalDistance < horizontalDistance)
+        {
+            _verticalCamera.depth = -2;
+            _horizontalCamera.depth = -4;
+            _diagonalCamera.depth = -3;
+        }
+
+        if (diagonalDistance < verticalDistance && verticalDistance < horizontalDistance)
+        {
+            _verticalCamera.depth = -3;
+            _horizontalCamera.depth = -4;
+            _diagonalCamera.depth = -2;
+        }
+
+        if (diagonalDistance < horizontalDistance && horizontalDistance < verticalDistance)
+        {
+            _verticalCamera.depth = -4;
+            _horizontalCamera.depth = -3;
+            _diagonalCamera.depth = -2;
+        }
+
+        if (horizontalDistance < verticalDistance && verticalDistance < diagonalDistance)
+        {
+            _verticalCamera.depth = -3;
+            _horizontalCamera.depth = -2;
+            _diagonalCamera.depth = -4;
+        }
+
+        if (horizontalDistance < diagonalDistance && diagonalDistance < verticalDistance)
+        {
+            _verticalCamera.depth = -4;
+            _horizontalCamera.depth = -2;
+            _diagonalCamera.depth = -2;
+        }
+
     }
 }
